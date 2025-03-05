3e0e799b41db64103a9db3f7c50dcb547b51a2e9
DG5DX <dafecano@gmail.com>
2025-03-05 10:59:38 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ import routerProducto from './routers/Productos.js'
 const app= express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static("public"))
 
 app.use("/factus/cliente", routerCliente)
 app.use("/factus/factura", routerFactura)
