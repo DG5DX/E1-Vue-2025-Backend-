8d6fad45130f0e3e313c96f60c772431b43592e0
DG5DX <dafecano@gmail.com>
2025-04-07 15:04:53 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ import routerCliente from "./routers/Clientes.js";
 import routerFactura from "./routers/Factura.js";
 import routerProducto from "./routers/Productos.js";
 
+dotenv.config(); // Cargar variables antes de usarlas
+
 const app = express();
+const PORT = process.env.PORT || 5000; // Puerto dinámico o 3000 local
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
@@ -16,12 +20,10 @@ app.use("/factus/cliente", routerCliente);
 app.use("/factus/factura", routerFactura);
 app.use("/factus/producto", routerProducto);
 
-dotenv.config();
-
-app.listen(process.env.PORT, () => {
-	console.log("Escuchando en el puerto", process.env.PORT);
+app.listen(PORT, () => {
+	console.log("Escuchando en el puerto", PORT);
 	mongoose
 		.connect(process.env.CNX_MONGO)
-		.then(() => console.log("conected"))
-		.catch((error) => console.log(error));
+		.then(() => console.log("Conectado a MongoDB"))
+		.catch((error) => console.error("Error de conexión:", error));
 });
