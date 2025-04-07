dc2e228a3c4e9ba17e447ad95569cf0b52bf95a5
DG5DX <dafecano@gmail.com>
2025-04-07 14:46:37 -0500

    Add files via upload

--- models/Productos.js
+++ models/Productos.js
@@ -5,8 +5,7 @@ const productosSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   unit_measure_id: { type: Number, required: true },
-  standard_code_id: { type: Number, required: true },
-  
+  standard_code_id: { type: Number, required: true }
 });
 
 export default mongoose.model("Producto", productosSchema);
--- routers/Factura.js
+++ routers/Factura.js
@@ -1,5 +1,4 @@
 import Router from "express";
-
 import { postFactura, getFactura } from "../controllers/Factura.js";
 
 const router = Router();
